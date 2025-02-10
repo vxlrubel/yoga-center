0c6f7c3ba9ef7270e6726489dbd57ff0ecec0d29
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-11 00:58:03 +0600

    update blog details page.

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -1,5 +1,277 @@
 @extends('layouts.app')
-
+@push('title', 'Blog Details | Yoga Center')
 @section('content')
+
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="assets/img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Blog details</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	<!-- Blog Details Section -->
+	<section class="blog-details-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9">
+					<div class="blog-details">
+						<div class="blog-preview">
+							<img src="assets/img/blog/big.jpg" alt="">
+						</div>
+						<h2>Yoga Therapy</h2>
+						<div class="blog-meta"><p><i class="material-icons">alarm_on</i>120 Mins </p></div>
+						<div class="blog-meta"><p><i class="material-icons">people_outline</i>250 People Registered</p></div>
+						<p>With so many long-term benefits for health and beauty, many people choose Yoga as a form of daily exercise and sports. However, for those who want to practice Yoga, especially beginners, the choice of location is extremely important. It is impossible to count how many gyms with a variety of quality and prices. Some of the following questions will be answered by Ahana Yoga to help you find the best practice room.</p>
+						<blockquote>Yoga is now one of the most popular practice in the world. It is not simply the movement of the four limbs, Yoga is the product of a massive culture, a special "religion" that brings great value.</blockquote>
+
+						<div class="blog-gallery">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="assets/img/blog/blog-gallery/1.jpg" alt="">
+									<img src="assets/img/blog/blog-gallery/4.jpg" alt="">
+								</div>
+								<div class="col-md-4">
+									<img src="assets/img/blog/blog-gallery/2.jpg" alt="">
+									<img src="assets/img/blog/blog-gallery/5.jpg" alt="">
+								</div>
+								<div class="col-md-4">
+									<img src="assets/img/blog/blog-gallery/3.jpg" alt="">
+								</div>
+							</div>
+						</div>
+						<h3>What reasons do we need to practice Yoga?</h3>
+						<p>Update the trend of 12 yoga exercises to help you lose weight effectively at home, help strengthen your exercise ability. Besides impacting weight loss effects, it also helps to repel diseases, enhance the body's resistance. Have a toned body with just 8 steps of yoga every day, helping to lose weight quickly combined with a reasonable diet.</p>
+						<div class="row">
+							<div class="col-sm-7">
+								<div class="ed-cata"><p><i class="material-icons">local_offer</i>Health & Beauty</p></div>
+							</div>
+							<div class="col-sm-5 text-left text-sm-right pt-4 pt-sm-0">
+								<div class="ed-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="blog-author-card">
+						<div class="blog-author">
+							<img src="assets/img/classes/author/4.jpg" alt="">
+							<div class="ba-text">
+								<h3>Dorothy Newman</h3>
+								<p>Let Yoga bring you perfect body, health and flexibility and a spirit of relaxation. Choose the nearest gym at Ahana Yoga and start this amazing journey!</p>
+								<div class="ba-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="blog-nav-warp text-center text-sm-left">
+						<div class="row">
+							<div class="col-sm-6">
+								<a href="#" class="blog-nav bn-prev text-decoration-none">
+									<i class="material-icons">keyboard_arrow_left</i>
+									<h3>Movements for Slim</h3>
+									<p>Previous Post</p>
+								</a>
+							</div>
+							<div class="col-sm-6 text-center text-sm-right">
+								<a href="#" class="blog-nav bn-next text-decoration-none">
+									<i class="material-icons">keyboard_arrow_right</i>
+									<h3>The Perfect Dose</h3>
+									<p>Next Post</p>
+								</a>
+							</div>
+						</div>
+					</div>
+					<h3 class="comment-title">Comments</h3>
+					<ul class="comment-list">
+						<li>
+							<img src="assets/img/classes/author/3.jpg" class="comment-pic" alt="">
+							<div class="comment-text">
+								<h3>Beverly Price </h3>
+								<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+								<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+								<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+							</div>
+							<ul class="comment-sub-list">
+								<li>
+									<img src="assets/img/classes/author/2.jpg" class="comment-pic" alt="">
+									<div class="comment-text">
+										<h3>Jacqueline Watkins</h3>
+										<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+										<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+										<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+									</div>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<img src="assets/img/classes/author/1.jpg" class="comment-pic" alt="">
+							<div class="comment-text">
+								<h3>Lori Gonzales</h3>
+								<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+								<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+								<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+							</div>
+						</li>
+					</ul>
+					<h3 class="comment-title">Leave a Reply</h3>
+					<form class="singup-form">
+						<div class="row">
+							<div class="col-md-6">
+								<input type="text" placeholder="First Name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Last Name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Your Email">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Phone Number">
+							</div>
+							<div class="col-md-12">
+								<textarea placeholder="Message"></textarea>
+								<a href="#" class="site-btn sb-gradient">Send message</a>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="col-lg-3 col-md-5 col-sm-8 sidebar">
+					<div class="sb-widget">
+						<form class="search-form">
+							<input type="text" placeholder="Search">
+							<button><i class="material-icons">search</i></button>
+						</form>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">About instructor</h2>
+						<div class="about-instructor-widget">
+							<img src="assets/img/classes/author/1.jpg" alt="">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University</p>
+							<div class="ai-social">
+								<a href="#" class="text-decoration-none"><i class="fa fa-facebook"></i></a>
+								<a href="#" class="text-decoration-none"><i class="fa fa-instagram"></i></a>
+								<a href="#" class="text-decoration-none"><i class="fa fa-twitter"></i></a>
+								<a href="#" class="text-decoration-none"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Categories</h2>
+						<ul class="sb-list">
+							<li><a href="#" class="text-decoration-none">Health<span>(5)</span></a></li>
+							<li><a href="#" class="text-decoration-none">Nature<span>(7)</span></a></li>
+							<li><a href="#" class="text-decoration-none">Beauty<span>(3)</span></a></li>
+							<li><a href="#" class="text-decoration-none">Challenge<span>(10)</span></a></li>
+							<li><a href="#" class="text-decoration-none">Training<span>(12)</span></a></li>
+							<li><a href="#" class="text-decoration-none">Progress<span>(4)</span></a></li>
+						</ul>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Latest post</h2>
+						<div class="latest-post-widget">
+							<div class="lp-item">
+								<div class="lp-thumb set-bg" data-setbg="assets/img/blog/8.jpg"></div>
+								<div class="lp-text">
+									<h3>Rapid Weight Loss</h3>
+									<p><i class="material-icons">event_available</i>June 28, 2019</p>
+								</div>
+							</div>
+							<div class="lp-item">
+								<div class="lp-thumb set-bg" data-setbg="assets/img/blog/3.jpg"></div>
+								<div class="lp-text">
+									<h3>Flexible Body</h3>
+									<p><i class="material-icons">event_available</i>June 28, 2019</p>
+								</div>
+							</div>
+							<div class="lp-item">
+								<div class="lp-thumb set-bg" data-setbg="assets/img/blog/9.jpg"></div>
+								<div class="lp-text">
+									<h3>Healthy Eating Mode</h3>
+									<p><i class="material-icons">event_available</i>June 28, 2019</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Latest post</h2>
+						<div class="insta-widget">
+							<div class="insta-item">
+								<div class="insta-img">
+									<img src="assets/img/infor/1.jpg" alt="">
+									<div class="insta-hover">
+										<i class="fa fa-instagram"></i>
+									</div>
+								</div>
+							</div>
+							<div class="insta-item">
+								<div class="insta-img">
+									<img src="assets/img/infor/2.jpg" alt="">
+									<div class="insta-hover">
+										<i class="fa fa-instagram"></i>
+									</div>
+								</div>
+							</div>
+							<div class="insta-item">
+								<div class="insta-img">
+									<img src="assets/img/infor/3.jpg" alt="">
+									<div class="insta-hover">
+										<i class="fa fa-instagram"></i>
+									</div>
+								</div>
+							</div>
+							<div class="insta-item">
+								<div class="insta-img">
+									<img src="assets/img/infor/4.jpg" alt="">
+									<div class="insta-hover">
+										<i class="fa fa-instagram"></i>
+									</div>
+								</div>
+							</div>
+							<div class="insta-item">
+								<div class="insta-img">
+									<img src="assets/img/infor/5.jpg" alt="">
+									<div class="insta-hover">
+										<i class="fa fa-instagram"></i>
+									</div>
+								</div>
+							</div>
+							<div class="insta-item">
+								<div class="insta-img">
+									<img src="assets/img/infor/6.jpg" alt="">
+									<div class="insta-hover">
+										<i class="fa fa-instagram"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Latest post</h2>
+						<div class="sb-tags">
+							<a href="#" class="text-decoration-none">Nature</a>
+							<a href="#" class="text-decoration-none">Health</a>
+							<a href="#" class="text-decoration-none">Yoga</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog Details Section end -->
 
 @endsection
