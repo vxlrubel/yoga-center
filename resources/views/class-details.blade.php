712b18040e699b4ca3c759c323cfc227670ba085
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-10 23:15:19 +0600

    update class details page.

--- resources/views/class-details.blade.php
+++ resources/views/class-details.blade.php
@@ -1,5 +1,228 @@
 @extends('layouts.app')
-
+@push('title', 'Class details | Yoga Center')
 @section('content')
+
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="assets/img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Our classes</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	<!-- Classes Details Section -->
+	<section class="classes-details-section spad overflow-hidden">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9">
+					<div class="classes-details">
+						<div class="classes-preview">
+							<img src="assets/img/classes/big.jpg" alt="">
+						</div>
+						<div class="row">
+							<div class="col-lg-8">
+								<h2>Yoga Therapy</h2>
+								<div class="cd-meta"><p><i class="material-icons">alarm_on</i>120 Mins </p></div>
+								<div class="cd-meta"><p><i class="material-icons">people_outline</i>250 People Registered</p></div>
+							</div>
+							<div class="col-lg-4 text-left text-md-right">
+								<div class="cd-price">$29</div>
+							</div>
+						</div>
+						<p>Yoga is the secret of many women in Vietnam and internationally. This is a very popular subject to bring extremely effective exercises to help improve physique effectively and reduce belly fat quickly. In order to have a perfect, toned physique, you need to arrange a suitable exercise regime for yourself as well as a nutritious diet. The following is a series of yoga exercises that help reduce fat effectively. 5 Moves to increase height for dwarf mushrooms. Exercise regularly to get the best results.</p>
+						<div class="row mb-5">
+							<div class="col-lg-5">
+								<img src="assets/img/classes/classes-details.png" alt="">
+							</div>
+							<div class="col-lg-7">
+								<blockquote>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain.</blockquote>
+								<h3>The secret to improving her height to achieve quick results:</h3>
+								<ul>
+									<li><i class="material-icons">done_all</i>Combining 30 minutes of practice and 30 jumps high</li>
+									<li><i class="material-icons">done_all</i>Apply scientific nutrition, avoid obesity.</li>
+									<li><i class="material-icons">done_all</i>Practice sports regularly</li>
+									<li><i class="material-icons">done_all</i>Various versions have evolved over the years</li>
+								</ul>
+							</div>
+						</div>
+						<p>Update the trend of 12 yoga exercises to help you lose weight effectively at home, help strengthen your exercise ability. Besides impacting weight loss effects, it also helps to repel diseases, enhance the body's resistance. Have a toned body with just 8 steps of yoga every day, helping to lose weight quickly combined with a reasonable diet.</p>
+						<div class="row">
+							<div class="col-sm-7">
+								<div class="cd-cata"><p><i class="material-icons">local_offer</i>Health & Beauty</p></div>
+							</div>
+							<div class="col-sm-5 text-left text-sm-right pt-4 pt-sm-0">
+								<div class="cd-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<h3 class="comment-title">Other Classes</h3>
+					<div class="classes-other-slider owl-carousel">
+						<div class="classes-item">
+							<div class="ci-img">
+								<img src="assets/img/classes/1.jpg" alt="">
+							</div>
+							<div class="ci-text">
+								<h4><a href="classes-details.html">Artistic Yoga</a></h4>
+								<div class="ci-metas">
+									<div class="ci-meta"><i class="material-icons">event_available</i>Mon, Wed, Fri</div>
+									<div class="ci-meta"><i class="material-icons">alarm_on</i>06:30pm - 07:45pm</div>
+								</div>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis</p>
+							</div>
+							<div class="ci-bottom">
+								<div class="ci-author">
+									<img src="assets/img/classes/author/1.jpg" alt="">
+									<div class="author-text">
+										<h6>Victoria Webb</h6>
+										<p>Yoga Trainer</p>
+									</div>
+								</div>
+								<a href="#" class="site-btn sb-gradient">book now</a>
+							</div>
+						</div>
+						<div class="classes-item">
+							<div class="ci-img">
+								<img src="assets/img/classes/2.jpg" alt="">
+							</div>
+							<div class="ci-text">
+								<h4>Traditional Hatha</h4>
+								<div class="ci-metas">
+									<div class="ci-meta"><i class="material-icons">event_available</i>Mon, Wed, Fri</div>
+									<div class="ci-meta"><i class="material-icons">alarm_on</i>06:30pm - 07:45pm</div>
+								</div>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis</p>
+							</div>
+							<div class="ci-bottom">
+								<div class="ci-author">
+									<img src="assets/img/classes/author/2.jpg" alt="">
+									<div class="author-text">
+										<h6>Victoria Webb</h6>
+										<p>Yoga Trainer</p>
+									</div>
+								</div>
+								<a href="#" class="site-btn sb-gradient">book now</a>
+							</div>
+						</div>
+						<div class="classes-item">
+							<div class="ci-img">
+								<img src="assets/img/classes/3.jpg" alt="">
+							</div>
+							<div class="ci-text">
+								<h4>Yoga Therapy</h4>
+								<div class="ci-metas">
+									<div class="ci-meta"><i class="material-icons">event_available</i>Mon, Wed, Fri</div>
+									<div class="ci-meta"><i class="material-icons">alarm_on</i>06:30pm - 07:45pm</div>
+								</div>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis</p>
+							</div>
+							<div class="ci-bottom">
+								<div class="ci-author">
+									<img src="assets/img/classes/author/3.jpg" alt="">
+									<div class="author-text">
+										<h6>Victoria Webb</h6>
+										<p>Yoga Trainer</p>
+									</div>
+								</div>
+								<a href="#" class="site-btn sb-gradient">book now</a>
+							</div>
+						</div>
+					</div>
+					<h3 class="comment-title">Comments</h3>
+					<ul class="comment-list">
+						<li>
+							<img src="assets/img/classes/author/3.jpg" class="comment-pic" alt="">
+							<div class="comment-text">
+								<h3>Beverly Price </h3>
+								<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+								<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+								<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+							</div>
+							<ul class="comment-sub-list">
+								<li>
+									<img src="assets/img/classes/author/2.jpg" class="comment-pic" alt="">
+									<div class="comment-text">
+										<h3>Jacqueline Watkins</h3>
+										<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+										<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+										<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+									</div>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<img src="assets/img/classes/author/1.jpg" class="comment-pic" alt="">
+							<div class="comment-text">
+								<h3>Lori Gonzales</h3>
+								<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+								<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+								<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+							</div>
+						</li>
+					</ul>
+					<h3 class="comment-title">Leave a Reply</h3>
+					<form class="singup-form">
+						<div class="row">
+							<div class="col-md-6">
+								<input type="text" placeholder="First Name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Last Name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Your Email">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Phone Number">
+							</div>
+							<div class="col-md-12">
+								<textarea placeholder="Message"></textarea>
+								<a href="#" class="site-btn sb-gradient">Send message</a>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="col-lg-3 col-md-5 col-sm-8 sidebar">
+					<div class="sb-widget">
+						<h2 class="sb-title">About instructor</h2>
+						<div class="classes-info">
+							<ul>
+								<li><i class="material-icons">map</i>184 Main Collins Street</li>
+								<li><i class="material-icons">event_available</i>15 January, 2019</li>
+								<li><i class="material-icons">done_all</i>Mon, Fri</li>
+								<li><i class="material-icons">alarm_on</i>06:30pm - 07:45pm</li>
+								<li><i class="material-icons">person_outline</i>Lori Kennedy</li>
+							</ul>
+						</div>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">About instructor</h2>
+						<div class="about-instructor-widget">
+							<img src="assets/img/classes/author/1.jpg" alt="">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University</p>
+							<div class="ai-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Classes Details Section end -->
 
 @endsection
