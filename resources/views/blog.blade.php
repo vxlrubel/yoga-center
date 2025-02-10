3a557a136b78dee0fd94d5edc1c88a2e8db4d312
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-11 00:50:27 +0600

    update blog page.

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,5 +1,185 @@
 @extends('layouts.app')
-
+@push('title', 'Our Blog | Yoga Center')
 @section('content')
+
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="assets/img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Blog gird</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	<!-- Events Section -->
+	<section class="events-page-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/1.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Yoga for Slim Physique</a></h2>
+							<p>Regular practice with breathing tech-niques in Yoga will help you get a toned body, this is the perfect choice</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/2.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">The Perfect Dose</a></h2>
+							<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/3.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Flexible Body</a></h2>
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis prae-sentium voluptatum deleniti</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/4.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">The Secret to Improving</a></h2>
+							<p>Regular practice with breathing tech-niques in Yoga will help you get a toned body, this is the perfect choice</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/5.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Movements for Slim</a></h2>
+							<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/6.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Update Training Trend</a></h2>
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis prae-sentium voluptatum deleniti</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/7.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Have a Toned Body</a></h2>
+							<p>Regular practice with breathing tech-niques in Yoga will help you get a toned body, this is the perfect choice</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/8.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Rapid Weight Loss</a></h2>
+							<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="assets/img/blog/9.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Healthy Eating Mode</a></h2>
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis prae-sentium voluptatum deleniti</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="site-pagination pt-3">
+				<a href="#" class="active">1</a>
+				<a href="#">2</a>
+				<a href="#"><i class="material-icons">keyboard_arrow_right</i></a>
+			</div>
+		</div>
+	</section>
+	<!-- Events Section end -->
 
 @endsection
