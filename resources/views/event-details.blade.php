e3d193d968c735ab359f546984c7a2f99c635e40
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-11 00:47:01 +0600

    update event details page.

--- resources/views/event-details.blade.php
+++ resources/views/event-details.blade.php
@@ -1,5 +1,198 @@
 @extends('layouts.app')
-
+@push('title', 'Event Details | Yoga Center')
 @section('content')
+
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="assets/img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Event details</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	<!-- Event Details Section -->
+	<section class="event-details-section spad overflow-hidden">
+		<div class="container">
+			<div class="event-preview">
+				<img src="assets/img/event/big.jpg" alt="">
+			</div>
+			<div class="row">
+				<div class="col-lg-9">
+					<div class="event-details">
+						<div class="row">
+							<div class="col-md-8">
+								<h2>Yoga Therapy</h2>
+								<div class="ed-meta"><p><i class="material-icons">alarm_on</i>120 Mins </p></div>
+								<div class="ed-meta"><p><i class="material-icons">people_outline</i>250 People Registered</p></div>
+							</div>
+							<div class="col-md-4 text-left text-md-right">
+								<div class="ed-note">Advanced</div>
+							</div>
+						</div>
+						<p>Yoga is the secret of many women in Vietnam and internationally. This is a very popular subject to bring extremely effective exercises to help improve physique effectively and reduce belly fat quickly. In order to have a perfect, toned physique, you need to arrange a suitable exercise regime for yourself as well as a nutritious diet. The following is a series of yoga exercises that help reduce fat effectively. 5 Moves to increase height for dwarf mushrooms. Exercise regularly to get the best results.</p>
+						<div class="row mb-5">
+							<div class="col-lg-6">
+								<div class="signup-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33245.297803635964!2d-73.76987401620775!3d40.704774398815005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1575866843291!5m2!1sen!2sbd" style="border:0;" allowfullscreen=""></iframe></div>
+							</div>
+							<div class="col-lg-6 pt-4 pb-5">
+								<h3>The secret to improving her height to achieve quick results:</h3>
+								<ul>
+									<li><i class="material-icons">done_all</i>Fast and healthy weight loss yoga</li>
+									<li><i class="material-icons">done_all</i>Yoga preserves beauty and youth</li>
+									<li><i class="material-icons">done_all</i> Yoga repels all diseases</li>
+									<li><i class="material-icons">done_all</i>Yoga brings peace, balance</li>
+									<li><i class="material-icons">done_all</i> Yoga exercises for beautiful belly slim waist</li>
+								</ul>
+							</div>
+						</div>
+						<p>Update the trend of 12 yoga exercises to help you lose weight effectively at home, help strengthen your exercise ability. Besides impacting weight loss effects, it also helps to repel diseases, enhance the body's resistance. Have a toned body with just 8 steps of yoga every day, helping to lose weight quickly combined with a reasonable diet.</p>
+						<div class="row">
+							<div class="col-lg-7">
+								<div class="ed-cata"><p><i class="material-icons">local_offer</i>Health & Beauty</p></div>
+							</div>
+							<div class="col-lg-5 text-left text-md-right">
+								<div class="ed-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<h3 class="comment-title">Other Classes</h3>
+					<div class="event-other-slider owl-carousel">
+						<div class="event-item">
+							<div class="ei-img">
+								<img src="assets/img/event/1.jpg" alt="">
+							</div>
+							<div class="ei-text">
+								<h4><a href="event-details.html">Free Yoga Madrid</a></h4>
+								<ul>
+									<li><i class="material-icons">person</i>Kelly Alexander</li>
+									<li><i class="material-icons">event_available</i>15 January, 2019</li>
+									<li><i class="material-icons">map</i>184 Main Collins Street</li>
+								</ul>
+							</div>
+						</div>
+						<div class="event-item">
+							<div class="ei-img">
+								<img src="assets/img/event/2.jpg" alt="">
+							</div>
+							<div class="ei-text">
+								<h4>Get on Your Mat</h4>
+								<ul>
+									<li><i class="material-icons">person</i>Kelly Alexander</li>
+									<li><i class="material-icons">event_available</i>15 January, 2019</li>
+									<li><i class="material-icons">map</i>184 Main Collins Street</li>
+								</ul>
+							</div>
+						</div>
+						<div class="event-item">
+							<div class="ei-img">
+								<img src="assets/img/event/3.jpg" alt="">
+							</div>
+							<div class="ei-text">
+								<h4>One Love Dallas</h4>
+								<ul>
+									<li><i class="material-icons">person</i>Kelly Alexander</li>
+									<li><i class="material-icons">event_available</i>15 January, 2019</li>
+									<li><i class="material-icons">map</i>184 Main Collins Street</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<h3 class="comment-title">Comments</h3>
+					<ul class="comment-list">
+						<li>
+							<img src="assets/img/classes/author/3.jpg" class="comment-pic" alt="">
+							<div class="comment-text">
+								<h3>Beverly Price </h3>
+								<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+								<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+								<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+							</div>
+							<ul class="comment-sub-list">
+								<li>
+									<img src="assets/img/classes/author/2.jpg" class="comment-pic" alt="">
+									<div class="comment-text">
+										<h3>Jacqueline Watkins</h3>
+										<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+										<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+										<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+									</div>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<img src="assets/img/classes/author/1.jpg" class="comment-pic" alt="">
+							<div class="comment-text">
+								<h3>Lori Gonzales</h3>
+								<div class="comment-date"><i class="material-icons">alarm_on</i>June 28, 2019 at 3:18 pm</div>
+								<p>Just practicing yoga for more than 3 months, I dropped from 64 kg to 58 kg. I have used many weight loss measures such as medication, diet, but the effects are slow and low. </p>
+								<a href="#" class="reply"><i class="material-icons">reply</i>Reply</a>
+							</div>
+						</li>
+					</ul>
+					<h3 class="comment-title">Leave a Reply</h3>
+					<form class="singup-form">
+						<div class="row">
+							<div class="col-md-6">
+								<input type="text" placeholder="First Name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Last Name">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Your Email">
+							</div>
+							<div class="col-md-6">
+								<input type="text" placeholder="Phone Number">
+							</div>
+							<div class="col-md-12">
+								<textarea placeholder="Message"></textarea>
+								<a href="#" class="site-btn sb-gradient">Send message</a>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="col-lg-3 col-md-5 col-sm-8 sidebar">
+					<div class="sb-widget">
+						<h2 class="sb-title">About instructor</h2>
+						<div class="classes-info">
+							<ul>
+								<li><i class="material-icons">map</i>184 Main Collins Street</li>
+								<li><i class="material-icons">event_available</i>15 January, 2019</li>
+								<li><i class="material-icons">done_all</i>Mon, Fri</li>
+								<li><i class="material-icons">alarm_on</i>06:30pm - 07:45pm</li>
+								<li><i class="material-icons">person_outline</i>Lori Kennedy</li>
+							</ul>
+						</div>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">About instructor</h2>
+						<div class="about-instructor-widget">
+							<img src="assets/img/classes/author/1.jpg" alt="">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University</p>
+							<div class="ai-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Event Details Section end -->
 
 @endsection
