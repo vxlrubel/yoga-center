40eb5e827e1d5b359148c8a6670bb0c7f08a7336
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-10 22:59:31 +0600

    update about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,329 @@
 @extends('layouts.app')
 
+@push('title', 'About yoga center | Yoga Center')
+
 @section('content')
+
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>About</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	<!-- Service Section -->
+	<section class="service-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-sm-6">
+					<div class="about-item icon-box">
+						<div class="ai-icon">
+							<img src="/assets/img/icons/about-1.png" alt="">
+						</div>
+						<div class="ai-text">
+							<h4>Full Rejuvenation</h4>
+							<p>Excepteur sint occaecat cupidatat non proident sunt</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="about-item icon-box">
+						<div class="ai-icon">
+							<img src="/assets/img/icons/about-2.png" alt="">
+						</div>
+						<div class="ai-text">
+							<h4>Extension of Spring</h4>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="about-item icon-box">
+						<div class="ai-icon">
+							<img src="/assets/img/icons/about-3.png" alt="">
+						</div>
+						<div class="ai-text">
+							<h4>Against Aging</h4>
+							<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="about-item icon-box">
+						<div class="ai-icon">
+							<img src="/assets/img/icons/about-4.png" alt="">
+						</div>
+						<div class="ai-text">
+							<h4>Slim Body</h4>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Service Section end -->
+
+	<!-- What we do Section -->
+	<section class="wwd-section spad set-bg" data-setbg="assets/img/wwd-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-6 col-lg-7 ms-auto">
+					<div class="wwd-text">
+						<h3>What we do</h3>
+						<p>To be invited to the nearest Cali center and get free physical advice to learn more about the program.</p>
+						<div class="single-progress-item">
+							<p>Breathing</p>
+							<div class="progress-bar-style" data-progress="84"  data-bgcolor="#f76d63"></div>
+						</div>
+						<div class="single-progress-item">
+							<p>Metabolism</p>
+							<div class="progress-bar-style" data-progress="75"  data-bgcolor="#f76d63"></div>
+						</div>
+						<div class="single-progress-item">
+							<p>Flexibility</p>
+							<div class="progress-bar-style" data-progress="90"  data-bgcolor="#f76d63"></div>
+						</div>
+						<div class="single-progress-item">
+							<p>Strongness</p>
+							<div class="progress-bar-style" data-progress="78"  data-bgcolor="#f76d63"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- What we do Section end -->
+
+	<!-- Trainer Section -->
+	<section class="trainer-section overflow-hidden spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<img src="/assets/img/icons/logo-icon.png" alt="">
+				<h2>Our Trainer Yoga</h2>
+				<p>Practice Yoga to perfect physical beauty, take care of your soul and enjoy life more fully!</p>
+			</div>
+			<div class="trainer-slider owl-carousel">
+				<div class="ts-item">
+					<div class="trainer-item">
+						<div class="ti-img">
+							<img src="/assets/img/trainer/1.png" alt="">
+						</div>
+						<div class="ti-text">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+							<div class="ti-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="ts-item">
+					<div class="trainer-item">
+						<div class="ti-img">
+							<img src="/assets/img/trainer/2.png" alt="">
+						</div>
+						<div class="ti-text">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+							<div class="ti-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="ts-item">
+					<div class="trainer-item">
+						<div class="ti-img">
+							<img src="/assets/img/trainer/3.png" alt="">
+						</div>
+						<div class="ti-text">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+							<div class="ti-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Trainer Section end -->
+
+	<!-- Review Section -->
+	<section class="review-section spad set-bg" data-setbg="img/review-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 m-auto">
+					<div class="review-slider owl-carousel">
+						<div class="review-item">
+							<div class="ri-img">
+								<img src="/assets/img/classes/author/1.jpg" alt="">
+							</div>
+							<div class="ri-text text-white">
+								<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness.</p>
+								<h4>Denise Thomas</h4>
+								<h6>Designer</h6>
+							</div>
+						</div>
+						<div class="review-item">
+							<div class="ri-img">
+								<img src="/assets/img/classes/author/2.jpg" alt="">
+							</div>
+							<div class="ri-text text-white">
+								<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness.</p>
+								<h4>Denise Thomas</h4>
+								<h6>Designer</h6>
+							</div>
+						</div>
+						<div class="review-item">
+							<div class="ri-img">
+								<img src="/assets/img/classes/author/3.jpg" alt="">
+							</div>
+							<div class="ri-text text-white">
+								<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness.</p>
+								<h4>Denise Thomas</h4>
+								<h6>Designer</h6>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Review Section end -->
+
+	<!-- Blog Section -->
+	<section class="about-blog-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<img src="/assets/img/icons/logo-icon.png" alt="">
+				<h2>Course benefits</h2>
+				<p>Practice Yoga to perfect physical beauty, take care of your soul and enjoy life more fully!</p>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="/assets/img/blog/1.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Yoga for Slim Physique</a></h2>
+							<p>Regular practice with breathing tech-niques in Yoga will help you get a toned body, this is the perfect choice</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="/assets/img/blog/2.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">The Perfect Dose</a></h2>
+							<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="blog-item">
+						<img src="/assets/img/blog/3.jpg" alt="">
+						<div class="bi-text">
+							<h2><a href="single-blog.html">Flexible Body</a></h2>
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis prae-sentium voluptatum deleniti</p>
+						</div>
+						<div class="bi-footer">
+							<div class="bi-cata">Health & Beauty</div>
+							<div class="bi-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog Section end -->
+
+	<!-- Gallery Section -->
+	<div class="gallery-section">
+		<div class="gallery-slider owl-carousel">
+			<div class="gs-item">
+				<img src="/assets/img/gallery/1.jpg" alt="">
+				<div class="gs-hover">
+					<i class="fa fa-instagram"></i>
+					<p>ahana.yoga</p>
+				</div>
+			</div>
+			<div class="gs-item">
+				<img src="/assets/img/gallery/2.jpg" alt="">
+				<div class="gs-hover">
+					<i class="fa fa-instagram"></i>
+					<p>ahana.yoga</p>
+				</div>
+			</div>
+			<div class="gs-item">
+				<img src="/assets/img/gallery/3.jpg" alt="">
+				<div class="gs-hover">
+					<i class="fa fa-instagram"></i>
+					<p>ahana.yoga</p>
+				</div>
+			</div>
+			<div class="gs-item">
+				<img src="/assets/img/gallery/4.jpg" alt="">
+				<div class="gs-hover">
+					<i class="fa fa-instagram"></i>
+					<p>ahana.yoga</p>
+				</div>
+			</div>
+			<div class="gs-item">
+				<img src="/assets/img/gallery/5.jpg" alt="">
+				<div class="gs-hover">
+					<i class="fa fa-instagram"></i>
+					<p>ahana.yoga</p>
+				</div>
+			</div>
+			<div class="gs-item">
+				<img src="/assets/img/gallery/6.jpg" alt="">
+				<div class="gs-hover">
+					<i class="fa fa-instagram"></i>
+					<p>ahana.yoga</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Gallery Section end -->
 
 @endsection
