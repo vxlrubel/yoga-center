55da067fda70efda3494acb099111b825df460b2
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-10 23:55:25 +0600

    update trainers page.

--- resources/views/trainers.blade.php
+++ resources/views/trainers.blade.php
@@ -1,5 +1,148 @@
 @extends('layouts.app')
-
+@push('title', 'Our Trainers | Yoga Center')
 @section('content')
+
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="assets/img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Our Trainers</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	<!-- Trainers Section -->
+	<section class="trainers-page-section spad overflow-hidden">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="trainer-item">
+						<div class="ti-img">
+							<img src="assets/img/trainer/1.png" alt="">
+						</div>
+						<div class="ti-text">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+							<div class="ti-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="trainer-item">
+						<div class="ti-img">
+							<img src="assets/img/trainer/2.png" alt="">
+						</div>
+						<div class="ti-text">
+							<h4>Lori Kennedy</h4>
+							<h6>Yoga Trainer</h6>
+							<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+							<div class="ti-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-linkedin"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="ts-item">
+						<div class="trainer-item">
+							<div class="ti-img">
+								<img src="assets/img/trainer/3.png" alt="">
+							</div>
+							<div class="ti-text">
+								<h4>Rebecca James</h4>
+								<h6>Yoga Trainer</h6>
+								<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+								<div class="ti-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="ts-item">
+						<div class="trainer-item">
+							<div class="ti-img">
+								<img src="assets/img/trainer/4.png" alt="">
+							</div>
+							<div class="ti-text">
+								<h4>Cynthia McCoy</h4>
+								<h6>Yoga Trainer</h6>
+								<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+								<div class="ti-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="ts-item">
+						<div class="trainer-item">
+							<div class="ti-img">
+								<img src="assets/img/trainer/5.png" alt="">
+							</div>
+							<div class="ti-text">
+								<h4>Olivia Richards</h4>
+								<h6>Yoga Trainer</h6>
+								<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+								<div class="ti-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="ts-item">
+						<div class="trainer-item">
+							<div class="ti-img">
+								<img src="assets/img/trainer/6.png" alt="">
+							</div>
+							<div class="ti-text">
+								<h4>Emily Walker</h4>
+								<h6>Yoga Trainer</h6>
+								<p>Yoga & Therapy Certificate of Uttarakhand University Sanskrit</p>
+								<div class="ti-social">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="site-pagination">
+				<a href="#" class="active">1</a>
+				<a href="#">2</a>
+				<a href="#"><i class="material-icons">keyboard_arrow_right</i></a>
+			</div>
+		</div>
+	</section>
+	<!-- Trainers Section end -->
 
 @endsection
