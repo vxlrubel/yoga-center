a06741a4e13e903ad559c7e9c402b7ca1a21f461
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-17 01:26:36 +0600

    update booking page.

--- resources/views/booknow.blade.php
+++ resources/views/booknow.blade.php
@@ -16,9 +16,47 @@
     <div class="container-xxl py-4">
         <div class="booking-container">
             <div class="left-sidebar">
-                <button type="button" title="Prev 7 Days" aria-pressed="false" class="btn btn-light rounded-0">back to booking</button>
+                <button type="button" title="Prev 7 Days" aria-pressed="false" class="btn btn-light rounded-0 mb-2">back to booking</button>
+                <div class="d-flex flex-wrap author-details">
+                    <div class="image">
+                        <div>
+                            <img src="https://cdn.pixabay.com/photo/2022/04/06/11/30/girl-7115394_1280.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="author-info">
+                        <div class="px-2">
+                            <h4 class="mb-1 text-capitalize fs-5 text-truncate">Catharina Anderson</h4>
+                            <strong>PHD</strong>
+                            <p class="m-0 info-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque atque dolorem aliquid, ut eveniet aut illo rem eligendi, iste voluptate, harum libero quae unde perferendis expedita ipsum optio quas! Quidem?
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <h3 class="my-3 fs-4">Select A treatment</h3>
+
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <div>Initial Visit</div>
+                        <div>60 min - $100.00</div>
+                        <div>Please contact us to book this</div>
+                        <div>Treatment. 844-DISTRICT</div>
+                        <div>demo@jane.clinic</div>
+                    </li>
+                    <li class="list-group-item active">
+                        <div>Subsequent Visit</div>
+                        <div>60 min - $100.00</div>
+                    </li>
+                    <li class="list-group-item">
+                        <div>Online Video Session</div>
+                        <div>60 min - $100.00 A remote video session</div>
+                    </li>
+                  </ul>
             </div>
-            <div class="right-sidebar">
+            <div class="border-bottom border-2 d-block w-100 my-3 d-md-none"></div>
+            <div class="right-sidebar ps-md-3">
                 <div id="calendar"></div>
             </div>
         </div>
